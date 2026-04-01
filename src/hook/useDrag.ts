8bd361dc2f47ee183e550cef38a994f1cd48448f
literate-t <gaekoon@gmail.com>
2026-04-01 12:16:03 +0900

    fix: 드래그가 끝나면 선택된 레이어를 해제한다

--- src/hook/useDrag.ts
+++ src/hook/useDrag.ts
@@ -38,11 +38,12 @@ export const useDrag = (layerId: string) => {
     function handleMouseUp() {
       if (isDragging.current) {
         isDragging.current = false;
+        setSelectedLayer(null);
         window.removeEventListener("mousemove", onMouseMove);
         window.removeEventListener("mouseup", handleMouseUp);
       }
     },
-    [onMouseMove],
+    [onMouseMove, setSelectedLayer],
   );
 
   const onMouseDown = (e: React.MouseEvent) => {

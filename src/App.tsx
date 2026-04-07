d88bb2f366ce6c162340776f4d69fc309164aceb
literate-t <gaekoon@gmail.com>
2026-04-07 22:05:07 +0900

    feat: Card3DContainer 컴포넌트를 테스트한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
-import EditorCanvas from "./component/EditorCanvas";
+import Card3DContainer from "./component/Card3DContainer";
 
 function App() {
   return (
-    <>
-      <EditorCanvas />
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      {/* <EditorCanvas /> */}
+
+      {/* Card3DContainer test */}
+      <Card3DContainer width={400} height={600}>
+        <div className="w-full h-full bg-linear-to-br from-blue-500 to-purple-300 flex items-center justify-center text-white text-2xl font-bold">
+          3D Tilt test
+        </div>
+      </Card3DContainer>
+    </div>
   );
 }
 

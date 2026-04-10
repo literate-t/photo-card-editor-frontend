a60cf4ae3c3e56dba6ee61869fc3b6f0574e667f
literate-t <gaekoon@gmail.com>
2026-04-10 21:47:37 +0900

    feat: 텍스트, 이미지 레이어를 생성할 수 있는 툴바를 추가하고, 3D 환경에서 결과물을 미리 볼 수 있는 프리뷰 모드를 추가한다

--- src/component/EditorCanvas.tsx
+++ src/component/EditorCanvas.tsx
@@ -1,14 +1,13 @@
 import { useEditorStore } from "../store/useEditorStore";
 import Card3DContainer from "./Card3DContainer";
 import LayerComponent from "./LayerComponent";
+import LayerToolbar from "./LayerToolbar";
 
 export default function EditorCanvas() {
   const layers = useEditorStore((state) => state.layers);
   const clearSelection = useEditorStore((state) => state.clearSelection);
   const isPreview = useEditorStore((state) => state.isPreview);
   const togglePreview = useEditorStore((state) => state.togglePreview);
-  // const addLayer = useEditorStore((state) => state.addLayer);
-  // const idRef = useRef<number>(0);
 
   const handleBackgroundClick = () => {
     if (!isPreview) {
@@ -16,31 +15,40 @@ export default function EditorCanvas() {
     }
   };
 
+  const Layer = (
+    <div className="relative w-full h-full bg-white overflow-hidden">
+      {layers.map((layer) => (
+        <LayerComponent key={layer.id} layerId={layer.id} />
+      ))}
+    </div>
+  );
+
   return (
     <div
       className="flex flex-col items-center justify-center w-full h-screen bg-gray-100"
       onMouseDown={handleBackgroundClick}
     >
       {/* 컨트롤 패널 */}
-      <div className="mb-6 space-x-4">
+      <div className="flex mb-6 space-x-4">
+        <LayerToolbar />
         <button
           onClick={(e) => {
             e.stopPropagation();
             togglePreview();
           }}
-          className="px-4 py-2 font-semibold text-white bg-indigo-600 rounded shadow hover:bg-indigo-700"
+          className="px-4 py-2 mt-4 font-semibold text-white bg-indigo-600 rounded shadow hover:bg-indigo-700"
         >
           {isPreview ? "편집모드로 돌아가기" : "미리보기"}
         </button>
       </div>
       {/* 3D 캔버스 영역 */}
-      <Card3DContainer width={400} height={600}>
-        <div className="relative w-full h-full bg-white overflow-hidden">
-          {layers.map((layer) => (
-            <LayerComponent key={layer.id} layerId={layer.id} />
-          ))}
-        </div>
-      </Card3DContainer>
+      {isPreview ? (
+        <Card3DContainer width={400} height={600}>
+          {Layer}
+        </Card3DContainer>
+      ) : (
+        <div className="w-100 h-150">{Layer}</div>
+      )}
     </div>
   );
 }

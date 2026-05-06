dd473cc4122c7791fd6c09f8707484c4d35b21b6
literate-t <gaekoon@gmail.com>
2026-05-07 04:27:51 +0900

    feat: 문구를 수정하고 테마 색상을 다크로 변경한다

--- src/page/CardPage.tsx
+++ src/page/CardPage.tsx
@@ -18,16 +18,13 @@ export default function CardPage() {
   // error
   if (isError) {
     return (
-      <div className="flex flex-col items-center justify-content w-full h-screen bg-gray-100">
-        <h1 className="text-2xl font-bold text-gray-800">
-          카드를 찾을 수 없습니다
-        </h1>
-        <p className="mt-2 text-gray-600">존재하지 않거나 삭제된 링크입니다</p>
+      <div className="flex flex-col items-center justify-center w-full h-screen bg-[#2a2b2d]">
+        <h1 className="text-2xl font-bold text-gray-100">Card not found.</h1>
         <Link
           to="/"
-          className="mt-4 px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700"
+          className="mt-4 px-4 py-2 text-white bg-indigo-500 rounded-xl hover:bg-blue-700"
         >
-          새 카드 만들기
+          Let's make some photo cards
         </Link>
       </div>
     );
@@ -35,7 +32,7 @@ export default function CardPage() {
 
   if (isLoading) {
     return (
-      <div className="flex flex-col items-center justify-content w-full h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center w-full h-screen bg-[#2a2b2d]">
         {/* 3D 컨테이너와 동일한 크기의 스켈레톤 */}
         <div className="w-100 h-150 bg-white rounded-lg shadow-md animate-pulse flex flex-col p-4">
           <div className="w-3/4 h-8  bg-gray-200 rounded" />
@@ -43,23 +40,23 @@ export default function CardPage() {
           <div className="w-1/2 h-6  bg-gray-200 rounded mt-4" />
         </div>
         <p className="mt-6 text-gray-500 font-medium animate-pulse">
-          3D 카드를 불러오는 중입니다...
+          Loading 3D card...
         </p>
       </div>
     );
   }
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-[#2a2b2d]">
       <Card3DContainer width={400} height={600}>
-        <div className="relative w-full h-full overflow-hidden bg-white">
+        <div className="relative w-full h-full overflow-hidden bg-gray-300">
           {layers.map((layer) => (
             <LayerComponent key={layer.id} layerId={layer.id} />
           ))}
         </div>
       </Card3DContainer>
       <Link to="/" className="text-gray-500 underline hover:text-gray-800">
-        포토카드 만들러 가기
+        Let's make some photo cards
       </Link>
     </div>
   );
